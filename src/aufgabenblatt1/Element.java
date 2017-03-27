121076926a48c40843f36ac362cb6bc33056dc9d
Leo Peters <leo.peters@haw-hamburg.de>
2017-03-27 22:08:35 +0200

    So, jetzt is alles beisammen. Ich liebe Git :D

---
+++ src/aufgabenblatt1/Element.java
@@ -0,0 +1,25 @@
+package aufgabenblatt1;
+
+public class Element {
+
+    private Element nextElement;
+    private Element prevElement;
+
+
+    public Element getNextElement() {
+	return nextElement;
+    }
+
+    public void setNextElement(Element nextElement) {
+	this.nextElement = nextElement;
+    }
+
+    public Element getPrevElement() {
+	return prevElement;
+    }
+
+    public void setPrevElement(Element previousElement) {
+	this.prevElement = previousElement;
+    }
+
+}
---
+++ src/aufgabenblatt1/List_Interface.java
@@ -0,0 +1,20 @@
+package aufgabenblatt1;
+
+import java.util.List;
+
+public interface List_Interface {
+
+    
+    public boolean insert(Element element, int pos);
+    
+    public boolean delete(int pos);
+    
+    public boolean delete(Element element);
+    
+    public int find(Element element);
+    
+    public Element retrieve(int pos);
+    
+    public boolean concat(List<Element> list2);
+    
+}
