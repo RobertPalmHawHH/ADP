dc42b7145b90df54961b793215db7e67d0de63b6
Leo Peters <leo.peters@haw-hamburg.de>
2017-03-28 17:39:07 +0200

    List_Interface List -> Liste umbenannt

--- src/aufgabenblatt1/Liste.java
+++ src/aufgabenblatt1/Liste.java
@@ -1,48 +1,49 @@
 package aufgabenblatt1;
 
-public class Liste{
-    
-    public static final int INIT_ARRAY_LENGTH = 16;
-    
-    private Element[] array;
-    private Element head;
-    private Element tail;
-    private int size;
-    
-    public Liste(){
-	array = new Element[INIT_ARRAY_LENGTH];
-	head = new Element();
-	tail = new Element();
-	head.setNextElement(tail);
-	tail.setPrevElement(head);
-	size = 0;
-    }
+public class Liste {
 
-    public Element[] getArray() {
-        return array;
-    }
+  public static final int INIT_ARRAY_LENGTH = 16;
 
-    public void setArray(Element[] array) {
-        this.array = array;
-    }
+  private Element[] array;
+  private Element head;
+  private Element tail;
+  private int size;
 
-    public Element getHead() {
-        return head;
-    }
+  public Liste() {
+    array = new Element[INIT_ARRAY_LENGTH];
+    head = new Element();
+    tail = new Element();
+    head.setNextElement(tail);
+    tail.setPrevElement(head);
+    size = 0;
+  }
 
-    public Element getTail() {
-        return tail;
-    }
-    
-    public int size(){
-	return size;
-    }
-    
-    public void increaseSize(){
-	size++;
-    }
-    public void decreaseSize(){
-	size--;
-    }
+  public Element[] getArray() {
+    return array;
+  }
+
+  public void setArray(Element[] array) {
+    this.array = array;
+  }
+
+  public Element getHead() {
+    return head;
+  }
+
+  public Element getTail() {
+    return tail;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public void increaseSize() {
+    size++;
+  }
+
+  public void decreaseSize() {
+    size--;
+  }
 
 }
