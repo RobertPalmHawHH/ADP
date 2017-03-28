b8a3c0c4fe253167e1094e0e3a82c3d2b40fced0
RobertPalmHawHH <robert.palm@haw-hamburg.de>
2017-03-28 17:26:49 +0200

    Listenklasse erweitert

---
+++ src/aufgabenblatt1/Liste.java
@@ -0,0 +1,37 @@
+package aufgabenblatt1;
+
+public class Liste{
+    
+    public static final int INIT_ARRAY_LENGTH = 16;
+    
+    private Element[] array;
+    private Element head;
+    private Element tail;
+    private int size;
+    
+    public Liste(){
+	array = new Element[INIT_ARRAY_LENGTH];
+	head = new Element();
+	tail = new Element();
+	head.setNextElement(tail);
+	tail.setPrevElement(head);
+	size = 0;
+    }
+
+    public Element[] getArray() {
+        return array;
+    }
+
+    public void setArray(Element[] array) {
+        this.array = array;
+    }
+
+    public Element getHead() {
+        return head;
+    }
+
+    public Element getTail() {
+        return tail;
+    }
+
+}
