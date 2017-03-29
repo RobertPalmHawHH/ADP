6cb8ef0bea1eeea258701a775b6795ab73202ab1
Leo Peters <leo.peters@haw-hamburg.de>
2017-03-29 18:07:05 +0200

    bla

--- src/aufgabenblatt1/ListeA.java
+++ src/aufgabenblatt1/ListeA.java
@@ -5,15 +5,14 @@ import java.util.List;
 import aufgabenblatt1.*;
 
 public class ListeA implements IList {
-  
+
   public static final int INIT_ARRAY_SIZE = 16;
   private int size;
   Position[] listArray = new Position[INIT_ARRAY_SIZE];
   private int zaehler;
-  
-  
-  public boolean concat(List<Element> list2) {
-    Element[] list2Array = new Element[list2.size()];
+
+  public boolean concat(IList list2) {
+    Position[] list2Array = new Position[list2.size()];
     list2.toArray(list2Array);
     int startPunkt = -1;
     int i = 0;
@@ -27,7 +26,7 @@ public class ListeA implements IList {
         startPunkt = listArray.length;
         increaseArraySize();
       }
-    } 
+    }
     while (listArray.length - startPunkt - list2Array.length <= 0) {
       increaseArraySize();
     }
@@ -37,276 +36,70 @@ public class ListeA implements IList {
     return true;
   }
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  public boolean delete(Liste list, int pos) {
-    if (posValid(list, pos)) {
-      System.out.println("Der angegebene Index liegt nicht innerhalb der Grenzen des Arrays");
-      return false;
-    }
-    for (int i = pos; i < lastArrayIndex; i++) {
-      listArray[i] = listArray[i + 1];
-    }
-    return true;
-  }
-  
-  public boolean delete(Element element) {
-    delete(find(element));
-    return true;
-  }
-  
-  public int find(Liste list, Key key) {
-    for (int i = 0; i < list.getArray().length; i++) {
-      if (list.getArray()[i].getKey() == key) {
-        return i;
-      }
-    }
-    System.out.println("Angegebenes Element wurde nicht gefunden. -1 zurueckgegeben");
-    return -1;
-  }
-
-  @Override
-  public aufgabenblatt1.List delete(Liste list, Key key) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-
-
-  public Element retrieve(Liste list, int pos) {
-    if (!posValid(list, pos)) {
-      System.out.println("Null zurueckgegeben!");
-      return null;
-    }
-    return list.getArray()[pos];
-  }
-  
-  @Override
-  public Element retriee(Liste list, int pos) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public aufgabenblatt1.List concat(Liste list, Element[] list2) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-
-
-
-
-
-
-
   public void increaseArraySize() {
     Position[] tempArray = new Position[listArray.length * 2];
     System.arraycopy(listArray, 0, tempArray, 0, listArray.length);
     listArray = tempArray;
   }
 
-//  private boolean posValid(Position pos) {
-//    if (pos >= list.getArray().length || pos < 0) {
-//      System.out.println("Position not in valid range!");
-//      return false;
-//    } else {
-//      return true;
-//    }
-//  }
-  
-  @Override
-  public boolean insert() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-  
+  // private boolean posValid(Position pos) {
+  // if (pos >= list.getArray().length || pos < 0) {
+  // System.out.println("Position not in valid range!");
+  // return false;
+  // } else {
+  // return true;
+  // }
+  // }
+
   @Override
   public boolean insert(Element element, Position pos) {
     if (listArray[listArray.length - 1] != null) {
       increaseArraySize();
     }
-    for (int i = 0; i < size; i++) {
-      if (listArray[i] == pos) {
-        
+    Position insertable = find(pos.getKEY());
+    for (int i = size - 1; i >= 0; i--) {
+      listArray[i] = listArray[i - 1];
+      if (listArray[i] == insertable) {
+        listArray[i] = new Position();
+        listArray[i].setElement(element);
+        return true;
       }
     }
-    listArray[pos] = element;
-    return listArray;
+    return false;
   }
 
   @Override
   public Position find(int key) {
-    listArray[0].getElement().getKey() = key;
-    for (int i = 0; 
+    for (int i = 0; i < size; i++) {
+      if (listArray[i].getKEY() == key) {
+        return listArray[i];
+      }
+    }
     return null;
   }
-
-
-
-
-
-
-
-
-
 
   @Override
   public boolean delete(Position pos) {
-    // TODO Auto-generated method stub
-    return false;
+    for (int i = 0; i < size - 1; i++) {
+      if (listArray[i] == pos) {
+        for (int j = i; j < size - 1; j++) {
+          listArray[j] = listArray[j + 1];
+          if (listArray[j + 1] == null) {
+            break;
+          }
+        }
+      }
+    }
+    return true;
   }
 
-
-
-
-
-
-
-
-
-
-
-
-  @Override
-  public boolean delete(String key) {
-    // TODO Auto-generated method stub
-    return false;
+  public boolean delete(int key) {
+    delete(find(key));
+    return true;
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   @Override
   public Element retrieve(Position pos) {
-    // TODO Auto-generated method stub
-    return null;
+    return pos.getElement();
   }
-
-
-
-
-
-
-
-
-
-
-
-
-  @Override
-  public boolean concat(IList list2) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  
-  
-  
-//  public void increaseArraySize(Element[] elements) {
-//    Element[] tempArray = new Element[listArray.length * 2];
-//    System.arraycopy(listArray, 0, tempArray, 0, listArray.length);
-//    listArray = tempArray;
-//    lastArrayIndex = listArray.length - 1;
-//  }
-//  
-//  
-//  
-//  
-//  
-//  
-//  
-//  
-//  public boolean posValid(Element[] elements, int pos) {
-//    if (pos >= listArray.length || pos < 0) {
-//      System.out.println("Position not in valid range!");
-//      return false;
-//    } else {
-//      return true;
-//    }
-//  }
-//  
-//  public int 
-//  public void checkSize(Element[] elements, int neededSize) {
-//    while (elements.length - elemCounter < neededSize) {
-//      increaseArraySize();
-//    }
-//  }
-//  @Override
-//  public Element[] insert(Element[] elements, Element element, int pos) {
-//    if (!posValid(elements, pos)) {
-//      System.out.println("Position not in valid range!");
-//      return elements;
-//    }
-//    if (listArray[lastArrayIndex] != null) {
-//      increaseArraySize();
-//    }
-//    for (int i = lastArrayIndex; i > pos; i++) {
-//      listArray[i] = listArray[i - 1];
-//    }
-//    listArray[pos] = element;
-//    return elements;
-//  }
-//    return null;
-//  }
-//
-//  @Override
-//  public Element[] delete(Element[] elements, int pos) {
-//    // TODO Auto-generated method stub
-//    return null;
-//  }
-//
-//  @Override
-//  public Element[] delete(Element[] elements, Key key) {
-//    // TODO Auto-generated method stub
-//    return null;
-//  }
-//
-//  @Override
-//  public int find(Element[] elements, Key key) {
-//    // TODO Auto-generated method stub
-//    return 0;
-//  }
-//
-//  @Override
-//  public Element retrieve(Element[] elements, int pos) {
-//    // TODO Auto-generated method stub
-//    return null;
-//  }
-//
-//  @Override
-//  public Element[] concat(Element[] list1, Element[] list2) {
-//    // TODO Auto-generated method stub
-//    return null;
-//  }
-//}
+}
