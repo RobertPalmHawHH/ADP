ef6f46ff3280ce1136121c0886c0acb080a68914
Leo Peters <leo.peters@haw-hamburg.de>
2017-04-04 14:31:23 +0200

    Fehler Schnelle Suche gefixt.

--- src/aufgabenblatt2/Main.java
+++ src/aufgabenblatt2/Main.java
@@ -2,7 +2,14 @@ package aufgabenblatt2;
 
 public class Main {
   public static void main(String args[]) {
-    LangsameSuche lSuche = new LangsameSuche();
-    lSuche.primzahlenLangsam(2560);
+//     LangsameSuche lSuche = new LangsameSuche();
+//     lSuche.primzahlenLangsam(2560);
+//    SchnelleSuche sSuche = new SchnelleSuche();
+    SiebSuche siSuche = new SiebSuche();
+//    siSuche.primzahlenSieb(40);
+    boolean[] a = siSuche.primzahlenSieb(50);
+    for (int i = 0; i < 50; i++) {
+      System.out.println(i + "  " + a[i]);
+    }
   }
 }
--- src/aufgabenblatt2/SchnelleSuche.java
+++ src/aufgabenblatt2/SchnelleSuche.java
@@ -2,6 +2,24 @@ package aufgabenblatt2;
 
 public class SchnelleSuche {
   private static int zaehlerSchnell;
-  
-  
+
+  public boolean[] primzahlenSchnell(int N) {
+    boolean[] a = new boolean[N];
+    // Initialisierung
+    for (int i = 0; i < N; i++) {
+      a[i] = true;
+    }
+    // Berechnung
+    for (int i = 3; i < N; i++) {
+      for (int j = 2; j <= Math.sqrt(i); j++) {
+        System.out.println("Langsam: " + zaehlerSchnell);
+        zaehlerSchnell++;
+        if ((i % j == 0) && (j != i)) {
+          a[i] = false;
+          break;
+        }
+      }
+    }
+    return a;
+  }
 }
